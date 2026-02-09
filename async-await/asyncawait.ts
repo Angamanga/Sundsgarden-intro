95c82b3011817c7dee5eb5784a6518ce76e3e594
Anna Iosif <anna@ushahidi.com>
2026-02-09 10:54:28 +0100

    Using async-await to fetch weather

--- async-await/asyncawait.ts
+++ async-await/asyncawait.ts
@@ -37,3 +37,38 @@ const playGame = async (gesture: Gesture) => {
 playGame("sax");
 playGame("pase");
 playGame("blaha");
+
+// Part 2: Fetching Data from an API with Async/Await  
+
+// Fetch and Handle Data
+/* helper-function to extract the year, month, day and our out 
+of the initial time set in the response, that is in the format
+yyyymmddhh, and then convert it to a date
+*/
+const parseInitialTime = (initTime: string): Date => {
+    const year = Number(initTime.slice(0, 4));
+    const month = Number(initTime.slice(4, 6));
+    const day = Number(initTime.slice(6, 8));
+    const hour = Number(initTime.slice(8, 10));
+    
+    return new Date(Date.UTC(year, month - 1, day, hour));
+};
+
+// Fetching data from an API, using async/await
+const fetchWeather = async () => {
+    console.log("Fetching weather....");
+    try {
+        const response = await fetch('https://www.7timer.info/bin/astro.php?lon=129.7319787&lat=62.0274078&ac=0&unit=metric&output=json&tzshift=0')
+        const data = await response.json()
+        // Getting a human readable date out of the initial time-stamp
+        let date = parseInitialTime(data.init);
+        // Adding the offset for the first forecast
+        date.setUTCHours(date.getUTCHours() + data.dataseries[0].timepoint);
+        console.log(`The weather in Yakutsk at ${date} was ${data.dataseries[0].temp2m} degrees Celcius`);
+    } catch (error) {
+          console.log("Oh no, something went wrong!\n");
+            console.log(`Error-response: ${error}`);  
+        };
+};
+
+fetchWeather();
