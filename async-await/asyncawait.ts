44be9d09e51146600ed6c5100d9a9ea055b1285a
Anna Iosif <anna@ushahidi.com>
2026-02-09 10:53:03 +0100

    Adding promise, a bit refactored

---
+++ async-await/asyncawait.ts
@@ -0,0 +1,25 @@
+// Part 1: Refactoring Your Own Promise with Async/Await  
+type Gesture = "sten" | "sax" | "pase";
+
+const gamePromise = (gesture: Gesture): Promise<string> => {
+    return new Promise((resolve, reject) => {
+        const gestures: Gesture[] = ["sten", "sax", "pase"];
+        const computerGesture = gestures[Math.floor(Math.random() * gestures.length)];
+        const outcomes = {
+            sten: { sax: "You won!", pase: "You lost", sten: "Tie!!" },
+            pase: { sten: "You won!", sax: "You lost", pase: "Tie!!" },
+            sax: { pase: "You won!", sten: "You lost", sax: "Tie!!" }
+        };
+
+        if (gesture in outcomes) {
+            const result = outcomes[gesture][computerGesture];
+            if (result === "You lost") {
+                reject(result);
+            } else {
+                resolve(result);
+            }
+        } else {
+            reject("You must choose sten, sax or pase");
+        }
+    });
+};
